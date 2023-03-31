78ffd8548dd535dab9f038917c744d86c57b93ca
Shiffff <alexis.privat@live.fr>
2023-03-31 22:56:51 +0200

    svgSauv

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -3,13 +3,111 @@ import React from "react";
 const Project = () => {
   const projects = [
     {
+      title: "Billed",
+      mission:
+        "Bugs identified through a kanban were corrected, unit testing was conducted using jestjs, and an end-to-end testing plan was created.",
+
+      outils: [
+        {
+          name: "javascript",
+        },
+        {
+          name: "jestjs",
+        },
+        {
+          name: "notion",
+        },
+      ],
+      link: "",
+    },
+    {
       title: "Professeur Coquille",
       mission:
         "A secure platform was developed for a teacher and their students using React, Node.js, Express, and MongoDB. The project was deployed on a Hostinger VPS using PM2 and Nginx, The platform allows users to login with credentials and share content similar to a social network, as well as provide courses to students.",
       outils: [
         {
-          name: "HTML",
-          pic: "../logo192.png",
+          name: "reactjs",
+        },
+        {
+          name: "sass",
+        },
+        {
+          name: "nodejs",
+        },
+        {
+          name: "redux",
+        },
+        {
+          name: "mongoDB",
+        },
+      ],
+      link: "",
+    },
+    {
+      title: "les petits plats",
+      mission:
+        "Two search algorithms were created with multiple filters, implemented through the responsibility pattern, with one version using native loops and another using functional programming with array methods. The performance of both versions was then tested.",
+      outils: [
+        {
+          name: "javascript",
+        },
+        {
+          name: "html",
+        },
+        {
+          name: "css",
+        },
+      ],
+      link: "",
+    },
+    {
+      title: "groupomania",
+      mission:
+        "Create a secure enterprise social network with authentication/token. Manage profiles, allow users to post content on the platform, follow specific users, and more.",
+      outils: [
+        {
+          name: "reactjs",
+        },
+        {
+          name: "nodejs",
+        },
+        {
+          name: "redux",
+        },
+        {
+          name: "sass",
+        },
+        {
+          name: "mongoDB",
+        },
+      ],
+      link: "",
+    },
+
+    {
+      title: "ohmyfood",
+      mission: "Creation of SCSS animations based on a design mockup.",
+      outils: [
+        {
+          name: "html",
+        },
+        {
+          name: "sass",
+        },
+      ],
+      link: "",
+    },
+
+    {
+      title: "gameon",
+      mission:
+        "Multiple bugs were fixed, and an registration modal was implemented.",
+      outils: [
+        {
+          name: "html",
+        },
+        {
+          name: "css",
         },
         {
           name: "javascript",
@@ -17,67 +115,20 @@ const Project = () => {
       ],
       link: "",
     },
+
     {
-      title: "test1",
-      picture: "../test1.png",
+      title: "fisheye",
       mission:
-        "A secure platform was developed for a teacher and their students using React, Node.js, Express, and MongoDB. The project was deployed on a Hostinger VPS using PM2 and Nginx. The platform allows users to login with credentials and share content similar to a social network, as well as provide courses to students.",
-
+        "Create an accessible website for a platform of photographers. Ensure logical navigation with tab support and use of aria-label tags.",
       outils: [
         {
-          name: "Name1",
+          name: "html",
         },
-      ],
-      link: "",
-    },
-    {
-      title: "test1",
-      mission: "fafazfaz fa az faz fa faz",
-      outils: [
         {
-          name: "Name1",
+          name: "css",
         },
-      ],
-      link: "",
-    },
-    {
-      title: "test1",
-      mission: "fafazfaz fa az faz fa faz",
-      outils: [
         {
-          name: "Name1",
-        },
-      ],
-      link: "",
-    },
-    {
-      title: "test1",
-      mission: "fafazfaz fa az faz fa faz",
-      outils: [
-        {
-          name: "Name1",
-        },
-      ],
-      link: "",
-    },
-
-    {
-      title: "test1",
-      mission: "fafazfaz fa az faz fa faz",
-      outils: [
-        {
-          name: "Name1",
-        },
-      ],
-      link: "",
-    },
-
-    {
-      title: "test1",
-      mission: "fafazfaz fa az faz fa faz",
-      outils: [
-        {
-          name: "Name1",
+          name: "javascript",
         },
       ],
       link: "",
@@ -86,9 +137,8 @@ const Project = () => {
 
   return (
     <div className="projectPage">
-      <h2 className="pageTitle">
-        PORTFOLIO <span className="pointTitle nexttitle">.</span>
-      </h2>
+      <h2 className="pageTitle">PORTFOLIO</h2>
+      <span className="pointTitle nexttitle">.</span>
       <div className="projectContainer">
         <ul>
           {projects.map((project, index) => (
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,12 +1,142 @@
 import React from "react";
 
 const Skills = () => {
-  const parenthese = "(   )";
+  const parentheseLeft = "<";
+  const parentheseRight = "/>";
+
+  const hardSkills = [
+    {
+      title: "Web",
+      skills: [
+        {
+          name: "mongodb",
+          pic: "../mongodb.svg",
+        },
+        {
+          name: "sass",
+          pic: "",
+        },
+        {
+          name: "javascript",
+          pic: "",
+        },
+        {
+          name: "react",
+          pic: "",
+        },
+        {
+          name: "bootstrap",
+          Pic: "",
+        },
+        {
+          name: "html",
+          pic: "",
+        },
+        {
+          name: "css",
+          pic: "",
+        },
+      ],
+    },
+    {
+      title: "Deployment",
+      skills: [
+        {
+          name: "Nginx",
+          pic: "",
+        },
+        {
+          name: "pm2",
+          pic: "",
+        },
+      ],
+    },
+    {
+      title: "Other",
+      skills: [
+        {
+          name: "git",
+          pic: "",
+        },
+        {
+          name: "figma",
+          pic: "",
+        },
+        {
+          name: "notion",
+          pic: "",
+        },
+        {
+          name: "vscode",
+          pic: "",
+        },
+        {
+          name: "photoshop",
+          pic: "",
+        },
+        {
+          name: "trello",
+          pic: "",
+        },
+      ],
+    },
+    {
+      title: "Soft Skill",
+      skills: [
+        {
+          name: "Teamwork",
+          pic: "",
+        },
+        {
+          name: "Communication",
+          pic: "",
+        },
+        {
+          name: "Motivation",
+          pic: "",
+        },
+        {
+          name: "Leadership",
+          pic: "",
+        },
+        {
+          name: "Empathy",
+          pic: "",
+        },
+
+        {
+          name: "Flexibility",
+          pic: "",
+        },
+      ],
+    },
+  ];
+
   return (
     <div className="skillsPage">
       <h2 className="pageTitle">
-        SKILLS <span className="pointTitle nexttitle">{parenthese}</span>
+        <span className=" nexttitle">{parentheseLeft}</span> SKILLS
+        <span className=" nexttitle">{parentheseRight}</span>
       </h2>
+      <div className="skillFullContainer">
+        <div className="skillContainer">
+          {hardSkills.map((hardskill, index) => (
+            <div key={index} className="hardSkillFullContainer">
+              <h4>{hardskill.title}</h4>
+              <div className="hardSkillContainer">
+                {hardskill.skills.map((skill, index) => (
+                  <div key={index} className="hardSkill">
+                    <div className="imgSkillContainer">
+                      <img src={hardskill.pic}></img>
+                    </div>
+                    {skill.name}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
